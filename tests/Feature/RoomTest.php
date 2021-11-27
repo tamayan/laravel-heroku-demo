02cc7a7fcbc9f8fe910755f3c72b1169226766e4
Yuki Kitagawa <kitagawa@tam-bourine.co.jp>
2021-11-28 00:29:45 +0900

    fix test

--- tests/Feature/RoomTest.php
+++ tests/Feature/RoomTest.php
@@ -10,13 +10,12 @@ class RoomTest extends TestCase
 {
     private const SINGLE = 'single';
 
+    private const SEMI_DOUBLE = 'semi-double';
+
     /**
-     * A basic feature test example.
-     *
-     * @return void
      * @throws Exception
      */
-    public function test_Success_Single()
+    public function test_success_single()
     {
         $room = new Room(self::SINGLE, 1);
 
@@ -25,15 +24,38 @@ class RoomTest extends TestCase
         $this->assertSame(1, $room->getCount());
     }
 
-    public function test_Fail_single_none() {
+    public function test_fail_single_none() {
         $this->expectException(Exception::class);
         // 0人をセットしてエラーとなる
         new Room(self::SINGLE, 0);
     }
 
-    public function test_Fail_single_limit() {
+    public function test_fail_single_limit() {
         $this->expectException(Exception::class);
         // 上限が1人なのでエラーとなる
         new Room(self::SINGLE, 2);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function test_success_semi_double() {
+        $room = new Room(self::SEMI_DOUBLE, 2);
+
+        $this->assertInstanceOf(Room::class, $room);
+        $this->assertSame(self::SEMI_DOUBLE, $room->getType());
+        $this->assertSame(2, $room->getCount());
+    }
+
+    public function test_Fail_semi_double_none() {
+        $this->expectException(Exception::class);
+        // 上限が1人なのでエラーとなる
+        new Room(self::SEMI_DOUBLE, 0);
+    }
+
+    public function test_Fail_semi_double_limit() {
+        $this->expectException(Exception::class);
+        // 上限が2人なのでエラーとなる
+        new Room(self::SEMI_DOUBLE, 3);
     }
 }
